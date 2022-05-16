0b6b4aa1c41016e0bef56ea0f54f9a1821d3bfb2
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-16 12:42:00 +0530

    [Madhavi] committed

---
+++ LineComparison/LineComparison/LineClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineClass
+    {
+        public double calculateLength(double x1,double x2,double y1,double y2)
+        {
+            double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return result;
+        }
+    }
+}
---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+LineComparison.LineClass line = new LineComparison.LineClass();
+double len1 = line.calculateLength(4, 2, 6, 4);
+Console.WriteLine("Length of line is " + len1);
